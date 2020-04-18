dc0be840255d476f870619f91fb3875fb89bde23
UnayOzan <unayozan@gmail.com>
2020-04-18 14:39:44 +0300

    sheep movement

---
+++ Assets/_Scripts/SheepBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SheepBehaviour : MonoBehaviour
+{
+    Transform target;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("SheepMovementTarget").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void FixedUpdate()
+    {
+        agent.SetDestination(target.position);
+        transform.rotation = Quaternion.identity;
+    }
+}
