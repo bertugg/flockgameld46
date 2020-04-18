cf08e524fe828f4322f0188d5f246bfbbeb29087
UnayOzan <unayozan@gmail.com>
2020-04-18 15:01:09 +0300

    player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController characterController;
+
+    public float speed = 6.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        moveDirection *= speed;
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
